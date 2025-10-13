607414877269b06c9f419dc12f34251e49726a4c
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-10-14 00:17:12 +0530

    changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,7 @@ import {sendWelcomeEmail} from "../emails/emailHandlers.js"
 import generateToken from "../lib/utils.js"
 
 import User from "../models/User.js"
-import bcrypt from "bcryptjs"
+import bcrypt from 'bcryptjs'
 import { ENV } from "../lib/env.js";
 import cloudinary from "../lib/cloudinary.js";
 
